b45d033fb29d958f7f18f0e3f800a6fa643efd7e
Cameron Poore <cameron@>
2016-06-03 14:49:03 -0700

    Partially working JavaScript fix. It will have the other half workin 100% of the time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,26 @@
 $(function(){
   $(".questionnaire").submit(function(event){
     event.preventDefault();
-    var ques1 = parseInt($("#ques1select option:selected").val());
-    var ques2 = parseInt($("#ques2select option:selected").val());
-    var ques3 = parseInt($("#ques3select option:selected").val());
-    var ques4 = parseInt($("#ques4select option:selected").val());
-    var ques5 = parseInt($("#ques5select option:selected").val());
-    var ques6 = parseInt($("#ques6select option:selected").val());
-    var vacation = ("test1")
-    var vacation1 = ("test2")
-    var vacation2 = ("test3")
-    var vacation3 = ("wasd")
+    var ques1 = parseInt($("#q1select option:selected").val());
+    var ques2 = parseInt($("#q2select option:selected").val());
+    var ques3 = parseInt($("#q3select option:selected").val());
+    var ques4 = parseInt($("#q4select option:selected").val());
+    var ques5 = parseInt($("#q5select option:selected").val());
+    var ques6 = parseInt($("#q6select option:selected").val());
+    var vacation = ("Sweden");
+    var vacation1 = ("Bahamas");
+    var vacation2 = ("Portland");
+    var vacation3 = ("Florida");
     var total = ques1 + ques2 + ques3 + ques4 + ques5
 
-    if ("!ques6select"){
-    }else if (total >=5 && total <10){
-
+    if ("!q6select"){
+    } if (total >=5 && total <10){
       $("#vacation").show();
-    } else if (total >=10 && total <15 && total !=12){
-
+    } else if (total >=10 && total <15){
       $("#vacation1").show();
-    } else if (total >=16 && total <22){
-
+    } else if (total >=16 && total <25){
       $("#vacation2").show();
-    } else if (total ===12){
-
+    } else if (total ===22){
       $("#vacation3").show();
     }
 
@@ -34,15 +30,15 @@ $(function(){
 
 
 
-    if (vacation = true){
-    $(".output").empty().append("Sweden")
-  } if (vacation1 = true){
-    $(".output").empty().append("#vacation1")
-  } if (vacation2 = true){
-    $(".output").empty().append("#vacation2")
-  } if (vacation3 = true){
-    $(".output").empty().append("Island")
-  }
+  //   if (vacation = true){
+  //   $(".output").empty().append("Sweden")
+  // } if (vacation1 = true){
+  //   $(".output").empty().append("Bahamas")
+  // } if (vacation2 = true){
+  //   $(".output").empty().append("Portland")
+  // } if (vacation3 = true){
+  //   $(".output").empty().append("Florida")
+  // }
 
 
 
