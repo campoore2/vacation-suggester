56df67c55ba1079b4ff8630a83ff828574039ad6
Cameron Poore <cameron@>
2016-06-03 10:46:43 -0700

    Rewrote JS still throwing error. Will fix to have working script that outputs correct destination.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,31 @@
-
-
-
 $(function(){
   $(".questionnaire").submit(function(event){
     event.preventDefault();
-    var ques1 = parseInt($("#q1select option:selected").val());
-    var ques2 = parseInt($("#q2select option:selected").val());
-    var ques3 = parseInt($("#q3select option:selected").val());
-    var ques4 = parseInt($("#q4select option:selected").val());
-    var ques5 = parseInt($("#q5select option:selected").val());
-    var ques6 = parseInt($("#q6select option:selected").val());
-    var vaca;
+    var ques1 = parseInt($("#ques1select option:selected").val());
+    var ques2 = parseInt($("#ques2select option:selected").val());
+    var ques3 = parseInt($("#ques3select option:selected").val());
+    var ques4 = parseInt($("#ques4select option:selected").val());
+    var ques5 = parseInt($("#ques5select option:selected").val());
+    var ques6 = parseInt($("#ques6select option:selected").val());
+    var vacation;
     var total = ques1 + ques2 + ques3 + ques4 + ques5;
-debugger;
-    if(!ques6) {
+
+    debugger;
+    if (!ques6) {
       if (total >= 5 && total <10){
-        vaca="Bob";
+        vacation = "test1";
       }
-        else if(total >= 10 && total < 15 && total !== 12){
-        vaca="Mary";
+      else if (total >=10 && total <15 && total !==12) {
+        vacation = "test2";
       }
-        else if(total >=15 && total <20) {
-        vaca="Larry";
+      else if (total >=15 && total <20){
+        vacation = "test3";
       }
-        else if (total === 12){
-        vaca="Kenny";
+      else if (total === 12){
+        vacation="test4";
       }
+      $(".output").empty().append("<div class='thumbnail'><div class='caption'><h3>You are: " + vacation + "</h3></div></div>");
 
-
-
-    $(".output").empty().append("<div class='thumbnail'><div class='caption'><h3>You are: " + vaca + "</h3></div></div>");
-
-  };
-});
+    }
+  })
+})
